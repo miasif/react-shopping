be67774a38fae6f1df8e3d9da217b3c0ca8c6d37
miasif <minhazul.islam.asif@gmail.com>
2022-01-22 00:17:38 +0600

    announcement

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  
+  `;
+
+function Announcement() {
+  return <Container>Super Deal</Container>;
+}
+
+export default Announcement;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
 
 function Home() {
   return (
     <div>
+      <Announcement />
+
       <Navbar />
     </div>
   );
