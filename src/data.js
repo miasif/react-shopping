ea4eafd4ce961fd6e6566fcd28d34c6ff3ab8951
miasif <minhazul.islam.asif@gmail.com>
2022-01-22 17:40:45 +0600

    category

--- src/data.js
+++ src/data.js
@@ -4,20 +4,38 @@ export const sliderItems = [
     img: "https://i.picsum.photos/id/1/5616/3744.jpg?hmac=kKHwwU8s46oNettHKwJ24qOlIAsWN9d2TtsXDoCWWsQ",
     title: "SUMMER SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
-    bg: "f5fafd",
+    bg: "f9f7f7",
   },
   {
     id: 2,
     img: "https://png.pngitem.com/pimgs/s/248-2483863_fashion-girl-png-pic-fashion-girl-illustration-png.png",
     title: "AUTUMN SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
-    bg: "f5fafd",
+    bg: "fcf9f9",
   },
   {
-    id: 2,
+    id: 3,
     img: "https://picsum.photos/200/300",
     title: "SPRING SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
-    bg: "f5fafd",
+    bg: "f7elea",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+    title: "SHIRT STYLE",
+  },
+  {
+    id: 2,
+    img: "https://png.pngitem.com/pimgs/s/248-2483863_fashion-girl-png-pic-fashion-girl-illustration-png.png",
+    title: "PANT SALE",
+  },
+  {
+    id: 3,
+    img: "https://picsum.photos/200/300",
+    title: "LIGHT JACKETS",
   },
 ];
