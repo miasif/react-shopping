677d1a4a2cbd633ef30939a9f7b3ea62d6161550
miasif <minhazul.islam.asif@gmail.com>
2022-01-27 18:01:40 +0600

    image

--- src/data.js
+++ src/data.js
@@ -1,89 +1,101 @@
-export const sliderItems = [{
-        id: 1,
-        img: "https://i.picsum.photos/id/1/5616/3744.jpg?hmac=kKHwwU8s46oNettHKwJ24qOlIAsWN9d2TtsXDoCWWsQ",
-        title: "SUMMER SALE",
-        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
-        bg: "f9f7f7",
-    },
-    {
-        id: 2,
-        img: "https://png.pngitem.com/pimgs/s/248-2483863_fashion-girl-png-pic-fashion-girl-illustration-png.png",
-        title: "AUTUMN SALE",
-        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
-        bg: "fcf9f9",
-    },
-    {
-        id: 3,
-        img: "https://picsum.photos/200/300",
-        title: "SPRING SALE",
-        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
-        bg: "f7elea",
-    },
+const Image1 = require("../src/Image/one.png");
+const Image2 = require("../src/Image/2.png");
+const Image3 = require("../src/Image/3.png");
+const Image4 = require("../src/Image/4.png");
+const Image5 = require("../src/Image/5.png");
+const Image6 = require("../src/Image/6.png");
+const Image7 = require("../src/Image/7.png");
+const Image8 = require("../src/Image/8.png");
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.picsum.photos/id/1/5616/3744.jpg?hmac=kKHwwU8s46oNettHKwJ24qOlIAsWN9d2TtsXDoCWWsQ",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "f9f7f7",
+  },
+  {
+    id: 2,
+    img: "https://png.pngitem.com/pimgs/s/248-2483863_fashion-girl-png-pic-fashion-girl-illustration-png.png",
+    title: "AUTUMN SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "fcf9f9",
+  },
+  {
+    id: 3,
+    img: "https://picsum.photos/200/300",
+    title: "SPRING SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "f7elea",
+  },
 ];
 
-export const categories = [{
-        id: 1,
-        img: "https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
-        title: "SHIRT STYLE",
-    },
-    {
-        id: 2,
-        img: "https://png.pngitem.com/pimgs/s/248-2483863_fashion-girl-png-pic-fashion-girl-illustration-png.png",
-        title: "PANT SALE",
-    },
-    {
-        id: 3,
-        img: "https://picsum.photos/200/300",
-        title: "LIGHT JACKETS",
-    },
+export const categories = [
+  {
+    id: 1,
+    img: "https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+    title: "SHIRT STYLE",
+  },
+  {
+    id: 2,
+    img: "https://png.pngitem.com/pimgs/s/248-2483863_fashion-girl-png-pic-fashion-girl-illustration-png.png",
+    title: "PANT SALE",
+  },
+  {
+    id: 3,
+    img: "https://picsum.photos/200/300",
+    title: "LIGHT JACKETS",
+  },
 ];
 
-export const popularProduct = [{
-        id: 1,
-        img: "https://picsum.photos/seed/picsum/300/300",
-    },
-    {
-        id: 2,
-        img: "https://picsum.photos/seed/picsum/300/300",
-    },
-    {
-        id: 3,
-        img: "https://picsum.photos/seed/picsum/300/300",
-    },
-    {
-        id: 4,
-        img: "https://picsum.photos/seed/picsum/300/300",
-    },
-    {
-        id: 5,
-        img: "https://picsum.photos/seed/picsum/300/300",
-    },
-    {
-        id: 6,
-        img: "https://picsum.photos/seed/picsum/300/300",
-    },
-    {
-        id: 7,
-        img: "https://picsum.photos/seed/picsum/300/300",
-    },
-    {
-        id: 8,
-        img: "https://picsum.photos/seed/picsum/300/300",
-    },
-    {
-        id: 9,
-        img: "https://picsum.photos/seed/picsum/300/300",
-    },
-    {
-        id: 10,
-        img: "https://picsum.photos/seed/picsum/300/300",
-    },
-    {
-        id: 11,
-        img: "https://picsum.photos/seed/picsum/300/300",
-    },
-    {
-        id: 12,
-        img: "https://picsum.photos/seed/picsum/300/300",
-    },
+export const popularProduct = [
+  {
+    id: 1,
+    img: Image,
+  },
+  //   {
+  //     id: 2,
+  //     img: "https://picsum.photos/seed/picsum/300/300",
+  //   },
+  //   {
+  //     id: 3,
+  //     img: "https://picsum.photos/seed/picsum/300/300",
+  //   },
+  //   {
+  //     id: 4,
+  //     img: "https://picsum.photos/seed/picsum/300/300",
+  //   },
+  //   {
+  //     id: 5,
+  //     img: "https://picsum.photos/seed/picsum/300/300",
+  //   },
+  //   {
+  //     id: 6,
+  //     img: "https://picsum.photos/seed/picsum/300/300",
+  //   },
+  //   {
+  //     id: 7,
+  //     img: "https://picsum.photos/seed/picsum/300/300",
+  //   },
+  //   {
+  //     id: 8,
+  //     img: "https://picsum.photos/seed/picsum/300/300",
+  //   },
+  //   {
+  //     id: 9,
+  //     img: "https://picsum.photos/seed/picsum/300/300",
+  //   },
+  //   {
+  //     id: 10,
+  //     img: "https://picsum.photos/seed/picsum/300/300",
+  //   },
+  //   {
+  //     id: 11,
+  //     img: "https://picsum.photos/seed/picsum/300/300",
+  //   },
+  //   {
+  //     id: 12,
+  //     img: "https://picsum.photos/seed/picsum/300/300",
+  //   },
 ];
